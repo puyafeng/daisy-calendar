2062ec9c0ee75f94765f24fa671cad89c1db43e7
puyafeng <390871804@qq.com>
2021-06-15 14:13:56 +0800

    rebuild calendar

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	// 修改 src 目录 为 examples 目录
 	pages: {
 		index: {
-			entry: "src/main.js",
+			entry: "examples/main.js",
 			template: "public/index.html",
 			filename: "index.html"
 		}
